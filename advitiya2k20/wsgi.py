8fc781f736a4aaa5871d719435eb757fafccec57
dilip640 <dilipsharma640@gmail.com>
2019-10-10 18:46:51 +0530

    Load env in wsgi.py

--- advitiya2k20/wsgi.py
+++ advitiya2k20/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from dotenv import load_dotenv
+
+load_dotenv('Advitiya-20/.env')
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'advitiya2k20.settings')
 
 application = get_wsgi_application()
