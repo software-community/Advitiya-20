babbc6b2814b9fe23487d37e839ff8d1abbf1c95
dilip640 <dilipsharma640@gmail.com>
2019-10-11 03:22:20 +0530

    Add postgres to docker

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,24 @@
-FROM python:3.7
+FROM python:3.7.4-alpine
 
+ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
-RUN mkdir /code
-WORKDIR /code
-COPY requirements.txt /code/
+
+RUN apk update \
+    && apk add --virtual build-deps gcc python3-dev musl-dev \
+    && apk add postgresql-dev \
+    && pip install psycopg2 \
+    && apk del build-deps
+
+RUN mkdir /usr/src/app
+WORKDIR /usr/src/app
+COPY requirements.txt /usr/src/app
 RUN pip install -r requirements.txt
-COPY . /code/
+
+# copy entrypoint.sh
+COPY ./entrypoint.sh /usr/src/app/entrypoint.sh
+
+# copy project
+COPY . /usr/src/app/
+
+# run entrypoint.sh
+ENTRYPOINT ["/usr/src/app/entrypoint.sh"]
--- advitiya2k20/settings.py
+++ advitiya2k20/settings.py
@@ -70,10 +70,12 @@ if DEBUG:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql', 
-            'NAME': os.getenv('DB_NAME'),
-            'USER': os.getenv('DB_USER'),
-            'PASSWORD': os.getenv('DB_PASS'),
+            'ENGINE': os.environ.get('SQL_ENGINE', 'django.db.backends.sqlite3'),
+            'NAME': os.environ.get('SQL_DATABASE', os.path.join(BASE_DIR, 'db.sqlite3')),
+            'USER': os.environ.get('SQL_USER', 'user'),
+            'PASSWORD': os.environ.get('SQL_PASSWORD', 'password'),
+            'HOST': os.environ.get('SQL_HOST', 'localhost'),
+            'PORT': os.environ.get('SQL_PORT', '5432'),
         }
     }
 
@@ -116,8 +118,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_FILES = os.environ.get('STATIC_FILES', '')
-STATIC_ROOT = os.path.join(STATIC_FILES, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 AUTHENTICATION_BACKENDS = (
