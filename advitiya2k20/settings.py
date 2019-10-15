66817a8478d85af441980ba8aa32d6bd9c51e63c
dilip640 <dilipsharma640@gmail.com>
2019-10-15 21:45:17 +0530

    load env in setting

--- advitiya2k20/settings.py
+++ advitiya2k20/settings.py
@@ -1,5 +1,8 @@
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
