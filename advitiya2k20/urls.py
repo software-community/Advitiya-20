9195472c5d582de83ef91926e6f3c539b681f15b
dilip640 <dilipsharma640@gmail.com>
2019-10-20 23:40:32 +0530

    Add path to media file

--- advitiya2k20/urls.py
+++ advitiya2k20/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -7,6 +7,8 @@ urlpatterns = [
     # For Django All-Auth
     path('auth/', include('allauth.urls')),
     path('admin/', admin.site.urls),
-    path('ca/',include('ca.urls')),
-    path('',include('main_page.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('ca/', include('ca.urls')),
+    path('', include('main_page.urls')),
+] + static(settings.STATIC_URL,
+           document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,
+                                                        document_root=settings.MEDIA_ROOT)
--- main_page/models.py
+++ main_page/models.py
@@ -1,19 +1,30 @@
 from django.db import models
+from django.core.files.storage import FileSystemStorage
+from django.contrib.staticfiles.templatetags.staticfiles import static
+import os
 
 # Create your models here.
 
+
+def get_file_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = instance.name + '.' + ext
+    foldername = 'events'
+    return os.path.join(foldername, filename)
+
+
 CATEGORY_CHOCIES = (
-    ('1','Aeromodelling'),
-    ('2','Finance'),
-    ('3','Coding'),
-    ('4','Robotics'),
-    ('5','Automotive'),
-    ('6','CAD'),
-    ('7','Astronomy'),
-    ('8','Gaming'),
-    ('9','Quizzing'),
-    ('10','Entrepreneurship'),
-    ('11','Photo Editing'),
+    ('1', 'Aeromodelling'),
+    ('2', 'Finance'),
+    ('3', 'Coding'),
+    ('4', 'Robotics'),
+    ('5', 'Automotive'),
+    ('6', 'CAD'),
+    ('7', 'Astronomy'),
+    ('8', 'Gaming'),
+    ('9', 'Quizzing'),
+    ('10', 'Entrepreneurship'),
+    ('11', 'Photo Editing'),
 )
 
 
@@ -22,23 +33,25 @@ class Coordinator(models.Model):
     phone = models.CharField(max_length=10)
 
     def __str__(self):
-        return self.name +"\t"+self.phone
+        return self.name + "\t"+self.phone
+
 
 class Events(models.Model):
 
-    category = models.CharField(max_length=20, choices = CATEGORY_CHOCIES)
-    image = models.ImageField(null=True, blank=True)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOCIES)
+    image = models.ImageField(
+        upload_to=get_file_path, null=True, blank=True)
     name = models.CharField(max_length=30)
     description = models.TextField(null=True, blank=True)
     venue = models.CharField(max_length=100)
     team_lower_limit = models.IntegerField()
     team_upper_limit = models.IntegerField()
     fees = models.IntegerField()
-    coordinator = models.ForeignKey(Coordinator, on_delete = models.CASCADE)
+    coordinator = models.ForeignKey(Coordinator, on_delete=models.CASCADE)
     prize = models.IntegerField()
-    rulebook= models.URLField()
+    rulebook = models.URLField()
     start_date_time = models.DateTimeField(blank=False)
     end_date_time = models.DateTimeField(blank=False)
 
     def __str__(self):
-        return self.name+"\t"+self.coordinator.name+"\t"+str(self.date_time)
+        return self.name+"\t"+self.coordinator.name
