903662b4f7a3e21765d9695f07544d40ea6f82a1
Adarsh <2017csb1064@iitrpr.ac.in>
2019-10-30 23:43:44 +0530

    working on TSP registration

---
+++ TSP/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, redirect
+from django.conf.urls import include
+# from django.http import HttpResponseRedirect
+from django.contrib.auth import login, authenticate
+# from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.core.mail import send_mail
+from django.contrib.staticfiles.templatetags.staticfiles import static
+import os
+
+from ca import models
+from .forms import registerForm
+# Create your views here.
+def home(request):
+    return render(request, 'TSP/index.html')
+
+
+@login_required(login_url='/auth/google/login/')
+def register_profile(request):
+    person = models.Profile.objects.filter(user=request.user)
+    if(person.count()):
+        return redirect('ca:profile')
+    else:
+        form = registerForm(request.POST or None)
+    if(request.method == 'POST'):
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.user = request.user
+            instance.save()
+            send_mail(subject='Successful Registration for Campus Ambassador program for Advitiya 2020',
+                      message='',
+                      from_email=os.environ.get(
+                          'EMAIL_HOST_USER', ''),
+                      recipient_list=[instance.user.email],
+                      fail_silently=True,
+                      html_message='Dear ' + str(request.user.get_full_name()) +
+                      ',<br><br>You are successfully registered for Campus Ambassador program for Advitiya 2020.' +
+                      'We are excited for your journey with us.<br><br>Your CAMPUS AMBASSADOR CODE is <b>' +
+                      str(instance.ca_code) +
+                      '.</b><br>Please read the Campus Ambassador Policy here - https://'
+                      + request.get_host() + static('ca/ca.pdf') + '<br><br>We wish you best ' +
+                      'of luck. Give your best and earn exciting prizes !!!<br><br>Regards<br>Advitiya 2020 ' +
+                      'Public Relations Team')
+            return redirect('ca:profile')
+    return render(request, "ca/register.html", {"form": form, 'person': person})
