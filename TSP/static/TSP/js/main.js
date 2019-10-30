254cce644d7860c7e65a8b281f00c73c75a91c5d
Adarsh <2017csb1064@iitrpr.ac.in>
2019-10-30 23:43:44 +0530

    working on TSP registration

---
+++ TSP/static/TSP/js/main.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.main-heading', {
+    // Waits 1000ms after typing "First"
+    strings: ['ADVITIYA 2020', 'Techno Student Program'],
+    typeSpeed: 100,
+    smartBackspace: true,
+    loop: true,
+    backDelay: 2000,
+});
