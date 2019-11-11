72fc240f0cdb35bb43146dc947b39f834f3038fa
Adarsh <2017csb1064@iitrpr.ac.in>
2019-11-12 00:46:56 +0530

    Basic Event Registration Modal

--- main_page/models.py
+++ main_page/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.core.files.storage import FileSystemStorage
 from django.contrib.staticfiles.templatetags.staticfiles import static
+from django.utils.crypto import get_random_string
 import os
 
 # Create your models here.
@@ -46,7 +48,7 @@ class Events(models.Model):
     venue = models.CharField(max_length=100)
     team_lower_limit = models.IntegerField()
     team_upper_limit = models.IntegerField()
-    fees = models.IntegerField()
+    fees = models.IntegerField(default = 400)
     coordinator = models.ForeignKey(Coordinator, on_delete=models.CASCADE)
     prize = models.IntegerField()
     rulebook = models.URLField()
@@ -55,3 +57,39 @@ class Events(models.Model):
 
     def __str__(self):
         return self.name+"\t"+self.coordinator.name
+
+class Participant(models.Model):
+
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    phone_number = models.CharField(max_length=10, default=None)
+    college_name = models.CharField(max_length=200, default='no college')
+    participant_code = models.CharField(
+        max_length=6, verbose_name='Participant Code', unique=True, default = unique_participant_code())
+    
+    def unique_participant_code(self):
+        code = 'ADV_' + get_random_string(length=6).upper()
+        while Participant.objects.filter(participant_code = code).exists():
+            code = 'ADV_' + get_random_string(length=6).upper()
+        return code
+
+class Payment(models.Model):
+
+    participant = models.OneToOneField(Participant, primary_key = True)
+    payment_request_id = models.CharField(max_length = 100, default = 'none')
+    transaction_id = models.CharField(max_length=100, default='none')
+
+class EventRegistration(models.Model):
+
+    event = models.ForeignKey(Events, on_delete = models.CASCADE)
+    participant = models.ForeignKey(Participant, on_delete = models.CASCADE)
+
+class Team(models.Model):
+
+    name = models.CharField(max_length = 100)
+    leader = models.ForeignKey(Participant, on_delete = models.CASCADE)
+    event = models.ForeignKey(Events, on_delete = models.CASCADE)
+
+class TeamHasMembers(models.Model):
+
+    team = models.ForeignKey(Team, on_delete = models.CASCADE)
+    participant = models.ForeignKey(Participant, on_delete = models.CASCADE)
