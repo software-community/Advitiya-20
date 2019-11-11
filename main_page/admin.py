713021c3c461b9da2d5101b52791d84e75847d42
Adarsh <2017csb1064@iitrpr.ac.in>
2019-11-12 02:24:47 +0530

    admin add tables

--- main_page/admin.py
+++ main_page/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from main_page.models import Events, Coordinator
+from main_page.models import Events, Coordinator, Participant, Payment, EventRegistration, Team, TeamHasMembers
 
 admin.site.register(Events)
 admin.site.register(Coordinator)
-
+admin.site.register(Participant)
+admin.site.register(Payment)
+admin.site.register(EventRegistration)
+admin.site.register(Team)
+admin.site.register(TeamHasMembers)
