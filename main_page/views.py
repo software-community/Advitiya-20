0e0332a5211dcdb13dd728d74e95f763ec2e3a11
Adarsh <2017csb1064@iitrpr.ac.in>
2019-11-14 00:28:34 +0530

    Events List Dynamic

--- main_page/views.py
+++ main_page/views.py
@@ -1,23 +1,25 @@
 from django.shortcuts import render
-from main_page.models import  Coordinator, Events
+from main_page.models import  Coordinator, Events, CATEGORY_CHOCIES
 
 # Create your views here.
 
 def index(request):
-    return render(request,'main_page/index.html')
+    return render(request,'main_page/index.html', {'CATEGORY_CHOCIES': CATEGORY_CHOCIES})
 
 def events(request):
-    return render(request,'main_page/events.html')
+    events_list = {}
+    for category in CATEGORY_CHOCIES:
+        events_list[category[1]] = Events.objects.filter(category = category[0])
+    return render(request,'main_page/events.html', {'CATEGORY_CHOCIES': CATEGORY_CHOCIES,
+        'events': events_list
+    })
 
 def accomodation(request):
-    return render(request,'main_page/accomodation.html')
+    return render(request,'main_page/accomodation.html', {'CATEGORY_CHOCIES': CATEGORY_CHOCIES})
 
 def event_page(request,num):
     context = {
-    'obj' : Events.objects.get(id=num)
+        'event' : Events.objects.get(id=num)
     }
     template_name='main_page/event1.html'
     return render(request,template_name,context=context)
-
-
-
