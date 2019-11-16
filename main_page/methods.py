9eb242aca30f3e73bfaaead792fac5f8a1d137ea
Adarsh <2017csb1064@iitrpr.ac.in>
2019-11-17 01:57:13 +0530

    registration

---
+++ main_page/methods.py
@@ -0,0 +1,21 @@
+from instamojo_wrapper import Instamojo
+import os
+import requests
+
+
+def payment_request(name, amount, purpose, email, mobile):
+    api = Instamojo(api_key=os.getenv('API_AUTH_KEY'),
+                    auth_token=os.getenv('API_AUTH_TOKEN'))
+
+    # Create a new Payment Request
+    response = api.payment_request_create(
+        buyer_name=name,
+        amount=amount,
+        purpose=purpose,
+        send_email=True,
+        email=email,
+        phone=mobile,
+        redirect_url="https://advitiya.in/payment_redirect/",
+        webhook="https://advitiya.in/webhook/"
+    )
+    return response
--- main_page/urls.py
+++ main_page/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import views as auth_views
 from . import views
 app_name='main_page'
 
@@ -9,4 +11,9 @@ urlpatterns=[
     path('accomodation/',views.accomodation, name='accomodation'),
     path('events/<int:num>/',views.event_page,name='event_page'),
     path('events/registration',views.registration_page,name='registration_page'),
+    path('register-as-participant', views.registerAsParticipant, name = 'register_as_participant'),
+    path('register/<int:event_id>', views.registerForEvent, name = 'register_for_event'),
+    path('payment_redirect/', views.payment_redirect, name = 'payment_redirect'),
+    path('webhook/', csrf_exempt(views.webhook), name= 'webhook'),
+    path('accounts/google/logout/', auth_views.LogoutView.as_view(), name = "account_logout"),
 ]
