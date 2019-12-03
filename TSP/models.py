a57c4f38cf2dd2d9747a73b39ee57370fd26fd62
dilip640 <dilipsharma640@gmail.com>
2019-12-03 23:30:25 +0530

    merger master

--- TSP/models.py
+++ TSP/models.py
@@ -4,6 +4,8 @@ from django.core.validators import RegexValidator
 from django.contrib.auth.models import User
 from django.utils.crypto import get_random_string
 
+import os
+
 
 class Profile(models.Model):
     school_name = models.CharField(max_length=150, blank=False)
@@ -23,3 +25,19 @@ class Profile(models.Model):
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
+
+class Payment(models.Model):
+    profile = models.ForeignKey(Profile, on_delete = models.CASCADE)
+    category_a = models.PositiveSmallIntegerField(verbose_name="Category A(9-10th class)")
+    category_b = models.PositiveSmallIntegerField(verbose_name="Category B(11-12th class)")
+    payment_request_id = models.CharField(max_length = 100, default = 'none')
+    transaction_id = models.CharField(max_length=100, default='none')
+
+    def __str__(self):
+        return self.profile.user.get_full_name()
+
+    def get_total_payment(self):
+        category_a_fee = int(os.environ.get('CATEGORY_A_FEE', '50'))
+        category_b_fee = int(os.environ.get('CATEGORY_B_FEE', '50'))
+
+        return self.category_a*category_a_fee + self.category_b*category_b_fee
