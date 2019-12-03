e53e9b1be0640e6d1c8329ca2b2526f825af93a4
dilip640 <dilipsharma640@gmail.com>
2019-12-03 23:24:56 +0530

    Payment for TSP

--- TSP/admin.py
+++ TSP/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from TSP.models import Profile
+from TSP.models import Profile, Payment
 
 class ProfileDetails(admin.ModelAdmin):
     list_display = [field.name for field in Profile._meta.fields]
 
 admin.site.register(Profile, ProfileDetails)
+
+class PaymentDetails(admin.ModelAdmin):
+    list_display = [field.name for field in Payment._meta.fields]
+
+admin.site.register(Payment, PaymentDetails)
