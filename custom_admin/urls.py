7bc1ba61812fea1fac62a8ad275f629e579032e3
Adarsh <2017csb1064@iitrpr.ac.in>
2020-01-01 10:29:37 +0530

    unregistered workshop participants

--- custom_admin/urls.py
+++ custom_admin/urls.py
@@ -6,4 +6,5 @@ app_name='custom_admin'
 urlpatterns=[
     path('gen-ca-csv/',views.gen_ca_csv, name='gen_ca_csv'),
     path('gen-participant-csv/',views.gen_participant_csv, name='gen_participant_csv'),
+    path('gen-workshop-unregistered-csv/',views.gen_workshop_unregistered_participants_csv, name='gen_workshop_unregistered_participants_csv'),
 ]
