7179109a100758a2ade81ac53a288b4cfd6feec1
dilip640 <dilipsharma640@gmail.com>
2020-01-15 02:48:55 +0530

    Add apis and notification feature

--- custom_admin/admin.py
+++ custom_admin/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from custom_admin.models import Notification
+
 # Register your models here.
+
+class NotificationAdminView(admin.ModelAdmin):
+    list_display = [field.name for field in Notification._meta.fields]
+
+admin.site.register(Notification, NotificationAdminView)
