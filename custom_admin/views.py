ec18a15ef010c913b23af67dcaf52c5a535ce8a2
dilip640 <dilipsharma640@gmail.com>
2020-01-16 22:28:22 +0530

    Fix import in events registration csv

--- custom_admin/views.py
+++ custom_admin/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.decorators import login_required
 import csv
 import datetime
 from rest_framework.response import Response
