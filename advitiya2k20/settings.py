cf8fd5cf884be63f5480e087b960a552c3987b33
dilip640 <dilipsharma640@gmail.com>
2020-01-19 23:42:15 +0530

    Fix register event

--- advitiya2k20/settings.py
+++ advitiya2k20/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
     # rest framework
     'rest_framework',
+    'fcm_django',
 ]
 
 MIDDLEWARE = [
@@ -159,3 +160,7 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
+FCM_DJANGO_SETTINGS = {
+        "FCM_SERVER_KEY":os.environ.get('FCM_KEY', '')
+}
