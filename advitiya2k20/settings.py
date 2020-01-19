c04ac2045e7b45823189392c07e68fc283647b94
dilip640 <dilipsharma640@gmail.com>
2020-01-20 01:28:48 +0530

    Add logging

--- advitiya2k20/settings.py
+++ advitiya2k20/settings.py
@@ -164,3 +164,29 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 FCM_DJANGO_SETTINGS = {
         "FCM_SERVER_KEY":os.environ.get('FCM_KEY', '')
 }
+if not DEBUG:
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': True,
+        'formatters': {
+            'verbose': {
+                'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s'
+            }
+        },
+        'handlers': {
+            'gunicorn': {
+                'level': 'DEBUG',
+                'class': 'logging.handlers.RotatingFileHandler',
+                'formatter': 'verbose',
+                'filename': '/opt/djangoprojects/reports/bin/gunicorn.errors',
+                'maxBytes': 1024 * 1024 * 100,  # 100 mb
+            }
+        },
+        'loggers': {
+            'gunicorn.errors': {
+                'level': 'DEBUG',
+                'handlers': ['gunicorn'],
+                'propagate': True,
+            },
+        }
+    }
