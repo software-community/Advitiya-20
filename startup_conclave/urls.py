f51c5bd9d60bda2c1c4c3578ca58d5bd322b8cf9
Adarsh <2017csb1064@iitrpr.ac.in>
2020-01-20 03:12:16 +0530

    add webhook url

--- startup_conclave/urls.py
+++ startup_conclave/urls.py
@@ -8,4 +8,6 @@ urlpatterns=[
     path('register/', views.registerForStartup, name = 'register_for_startup'),
     path('register_for_bootcamp/', views.registerForBootCamp, name = 'register_for_bootcamp'),
     path('pay_for_stall/', views.payForStall, name = 'pay_for_stall'),
+    path('payment_redirect/', views.payment_redirect, name = 'payment_redirect'),
+    path('webhook/', csrf_exempt(views.webhook), name= 'webhook'),
 ]
