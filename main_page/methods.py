c726f103752e43fae3a4a6dbd4be3e42bde8b763
dilip640 <dilipsharma640@gmail.com>
2020-01-21 23:37:59 +0530

    Fix workshop accomodation payment

--- main_page/methods.py
+++ main_page/methods.py
@@ -56,16 +56,18 @@ def workshop_accomodation_payment_request(name, amount, purpose, email, mobile, 
     # Create a new Payment Request
     if payment_detail:
         response = api.payment_request_status(payment_detail.payment_request_id)
-    else:
-        response = api.payment_request_create(
-            buyer_name=name,
-            amount=amount,
-            purpose=purpose,
-            send_email=True,
-            email=email,
-            phone=mobile,
-            redirect_url="https://advitiya.in/workshop_accomodation_payment_redirect/",
-            webhook="https://advitiya.in/workshop_accomodation_webhook/",
-            allow_repeated_payments=False
-        )
+        if response['success']:
+            return response
+
+    response = api.payment_request_create(
+        buyer_name=name,
+        amount=amount,
+        purpose=purpose,
+        send_email=True,
+        email=email,
+        phone=mobile,
+        redirect_url="https://advitiya.in/workshop_accomodation_payment_redirect/",
+        webhook="https://advitiya.in/workshop_accomodation_webhook/",
+        allow_repeated_payments=False
+    )
     return response
