403feb6455f24fb688292c5eba884007d667b0c5
Adarsh <2017csb1064@iitrpr.ac.in>
2020-02-04 19:16:28 +0530

    url for workshop accommodation

--- custom_admin/urls.py
+++ custom_admin/urls.py
@@ -34,4 +34,7 @@ urlpatterns=[
 
     path('refresh-payments', views.refresh_payments, name='refresh_payment'),
     path('refresh-payments/<int:refresh_id>', views.refresh_payments, name='refresh_payment'),
+
+    path('workshop_accommodation_csv', views.workshop_accommodation_csv,
+        name='workshop_accommodation_csv'),
 ]
