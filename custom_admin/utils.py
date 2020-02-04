1aace8e62de02101d3fd28ec8c92f5270dac798d
dilip640 <dilipsharma640@gmail.com>
2020-02-04 12:28:37 +0530

    Try to fix payments

--- custom_admin/utils.py
+++ custom_admin/utils.py
@@ -31,3 +31,21 @@ def sendNotification(title, message):
     }
     r = requests.post('https://fcm.googleapis.com//v1/projects/advitiya2020-719da/messages:send',
                       json=postdata, headers=headers)
+
+
+from instamojo_wrapper import Instamojo
+def check_payment(request_id, at_sudhir):
+    if at_sudhir:
+        api = Instamojo(api_key=os.getenv('WORKSHOP_API_AUTH_KEY'),
+                        auth_token=os.getenv('WORKSHOP_API_AUTH_TOKEN'))
+    else:
+        api = Instamojo(api_key=os.getenv('API_AUTH_KEY'),
+                    auth_token=os.getenv('API_AUTH_TOKEN'))
+
+    # Create a new Payment Request
+    response = api.payment_request_status(request_id)
+    if response['success'] and response['payment_request']['status'] == 'Completed':
+        for payment in response['payment_request']['payments']:
+            if payment['status'] == 'Credit':
+                return payment['payment_id']
+    return None
