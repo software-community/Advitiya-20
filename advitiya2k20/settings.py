e28597b70266f6cd8236075a273cd220159209fd
dilip640 <dilipsharma640@gmail.com>
2020-02-05 19:34:38 +0530

    remove logging

--- advitiya2k20/settings.py
+++ advitiya2k20/settings.py
@@ -167,29 +167,3 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 FCM_DJANGO_SETTINGS = {
         "FCM_SERVER_KEY":os.environ.get('FCM_KEY', '')
 }
-if not C_DEBUG:
-    LOGGING = {
-        'version': 1,
-        'disable_existing_loggers': False,
-        'formatters': {
-            'verbose': {
-                'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s'
-            }
-        },
-        'handlers': {
-            'gunicorn': {
-                'level': 'DEBUG',
-                'class': 'logging.handlers.RotatingFileHandler',
-                'formatter': 'verbose',
-                'filename': '/home/advitiya/gunicorn.errors',
-                'maxBytes': 1024 * 1024 * 100,  # 100 mb
-            }
-        },
-        'loggers': {
-            'gunicorn.errors': {
-                'level': 'DEBUG',
-                'handlers': ['gunicorn'],
-                'propagate': True,
-            },
-        }
-    }
