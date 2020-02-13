ca0f35b0585df0f2d9b866935e24612805587b0d
dilip640 <dilipsharma640@gmail.com>
2020-02-13 16:25:34 +0530

    Techconnect centers on home page

--- techconnect/urls.py
+++ techconnect/urls.py
@@ -4,7 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 app_name='techconnect'
 
 urlpatterns=[
-    path('',views.index,name="index"),
+    path('',views.centers,name="index"),
     path('register/',views.register,name="register"),
     path('about/',views.about,name="about"),
     path('contact/',views.contact,name="contact"),
